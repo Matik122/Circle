0cdc230ee3c91d8c64ce0b51d2d630a45bf987f9
Akulevich Matvei <matvei.akulevich@celadonsoft.com>
2023-03-20 23:26:05 +0400

    add ontriggerobserv

--- Assets/Scripts/Circle/CircleView.cs
+++ Assets/Scripts/Circle/CircleView.cs
@@ -1,11 +1,25 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using UniRx;
+using UniRx.Triggers;
 using UnityEngine;
 
 public class CircleView : MonoBehaviour
 {
     [SerializeField] private float _speed;
+    [SerializeField] private Collider _trigger;
+    private CompositeDisposable _disposable = new CompositeDisposable();
+
+    private void Start()
+    {
+        _trigger.OnTriggerEnterAsObservable()
+            .Subscribe(collider =>
+            {
+                collider.gameObject.SetActive(false);
+            }).AddTo(_disposable);
+    }
+
     public void SetPosition(Vector3 endPostion)
     {
         transform.position = Vector3.MoveTowards(transform.position, endPostion,_speed * Time.deltaTime);
